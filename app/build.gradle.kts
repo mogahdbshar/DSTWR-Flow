d3b0b016f0fcaf9cc07fa8735b6a295534dd39c3
mogahdbshar <mogahdbshar@gmail.com>
2026-09-26 00:29:24 +0300

    Align minimum Android version with production netstack

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.dstwr.flow"
-        minSdk = 24
+        minSdk = 29
         targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
